905b745e06ce705922d5b429cb76114cad722e84
Mustafa Emre Tatlici <m.emretatlici@icloud.com>
2024-12-24 13:56:52 +0300

    Home Header and Progress Circle

---
+++ Fitmore/ProgressCircleView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressCircleView.swift
+//  Fitmore
+//
+//  Created by Emre Tatlıcı on 24.12.2024.
+//
+
+import SwiftUI
+
+struct ProgressCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressCircleView()
+}
