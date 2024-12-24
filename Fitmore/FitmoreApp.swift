ae24161f87886e637229040890d9ab814d59b6d6
EmreTatlici <youremail@example.com>
2024-12-24 12:01:19 +0300

    Initial Commit

---
+++ Fitmore/FitmoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitmoreApp.swift
+//  Fitmore
+//
+//  Created by Emre Tatlıcı on 24.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct FitmoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
